d704cfe186f1d2243d56f3849d3069b3a8bb9909
Amartya <amartya.kumar2001@gmail.com>
2026-02-27 19:35:35 +0530

    feat: project-setup with tw & prettier

---
+++ ep-01/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-400 text-5xl">
+      Hello....
+      <Hero />
+    </div>
+  );
+};
+
+export default App;
